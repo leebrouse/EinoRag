0bb25e6cd7030059e0be49a960179bde4c4e03d0
leebrouse <15935115+leebrouse@user.noreply.gitee.com>
2025-08-18 18:25:33 +0800

    Eino RAG project init

---
+++ test/embedder_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	_ "github.com/leebrouse/eino/internal/config"
+	"github.com/leebrouse/eino/internal/embadding"
+	"github.com/spf13/viper"
+	"google.golang.org/genai"
+)
+
+func TestGeminiEmbedder_Real(t *testing.T) {
+	ctx := context.Background()
+
+	client, err := genai.NewClient(ctx, nil) // 会自动读取环境变量 API KEY
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+
+	log.Println("Gemini api_key:", viper.GetString("gemini.embedder"))
+
+	embedder, err := embadding.NewGeminiEmbedder(client, viper.GetString("gemini.embedder"))
+	if err != nil {
+		t.Fatalf("failed to create embedder: %v", err)
+	}
+
+	texts := []string{"hello world"}
+	vectors, err := embedder.EmbedStrings(ctx, texts)
+	if err != nil {
+		t.Fatalf("EmbedStrings failed: %v", err)
+	}
+
+	t.Logf("embedding result: %v", vectors)
+}
---
+++ test/indexer/indexer.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/cloudwego/eino-ext/components/embedding/ark"
+	"github.com/cloudwego/eino-ext/components/indexer/milvus"
+	"github.com/cloudwego/eino/schema"
+	"github.com/milvus-io/milvus-sdk-go/v2/client"
+)
+
+func main() {
+	// Get the environment variables
+	addr := "http://127.0.0.1:9000"
+	username := "minioadmin"
+	password := "minioadmin"
+	arkApiKey := os.Getenv("ARK_API_KEY")
+	arkModel := os.Getenv("ARK_MODEL")
+
+	// Create a client
+	ctx := context.Background()
+	cli, err := client.NewClient(ctx, client.Config{
+		Address:  addr,
+		Username: username,
+		Password: password,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+		return
+	}
+	defer cli.Close()
+
+	// Create an embedding model
+	emb, err := ark.NewEmbedder(ctx, &ark.EmbeddingConfig{
+		APIKey: arkApiKey,
+		Model:  arkModel,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create embedding: %v", err)
+		return
+	}
+
+	// Create an indexer
+	indexer, err := milvus.NewIndexer(ctx, &milvus.IndexerConfig{
+		Client:    cli,
+		Embedding: emb,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create indexer: %v", err)
+		return
+	}
+	log.Printf("Indexer created success")
+
+	// Store documents
+	docs := []*schema.Document{
+		{
+			ID:      "milvus-1",
+			Content: "milvus is an open-source vector database",
+			MetaData: map[string]any{
+				"h1": "milvus",
+				"h2": "open-source",
+				"h3": "vector database",
+			},
+		},
+		{
+			ID:      "milvus-2",
+			Content: "milvus is a distributed vector database",
+		},
+	}
+	ids, err := indexer.Store(ctx, docs)
+	if err != nil {
+		log.Fatalf("Failed to store: %v", err)
+		return
+	}
+	log.Printf("Store success, ids: %v", ids)
+}
